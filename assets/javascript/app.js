32b3fd854847e0a2dc917458abb09693c2915c09
Robert H <robertjameshardin@aol.com>
2019-02-22 17:56:06 -0600

    added psuedo-code to get started

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+/*
+1. Pick a theme for the trivia game
+2. Create questions with 1 correct and 3 incorrect answers.
+3. Create score counters of correct and incorrect picks for each answer selected
+4. Create game states that determine which portion of the game is displayed and when.
+5. Create on-click events for buttons that correspond with the question that is displayed to determine if the user picked correctly of not.
+6. Create timers for each question that gives the user 30 seconds to pick an answer.
+7. Make timer move game to next stage whether the user picks correctly, incorrectly, or not at all.
+8. Create a win page for each correct guess.
+9. Create a loss page for each incorrect guess that displays the correct answer.
+Bonus. Play audio files for each correct or incorrect answer, as well as at the beginning and end of the game.
+*/
