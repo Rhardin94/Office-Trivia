1402db2a1ff388d785a3cb73686e92f5e5eaa436
Robert H <robertjameshardin@aol.com>
2019-02-27 14:14:29 -0600

    new

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
     * If the user chooses incorrectly, a snarky screen with the correct answer is displayed, then the game moves to the next question
     * If the the user does not answer within the alloted time, another snarky screen is displayed, then the game moves to the next question
     * Finally, once the user has cycled through all the questions, a score screen is displayed that includes a button to restart the game.
+
 This project *showcases* my progress and understanding of **JavaScript** and **jQuery** applied to working applications.
 Solely contributed and maintained by **rhardin94(Robert Hardin)** with some advice and troubleshooting help from #TeamRoPo.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,145 +1,249 @@
-/*
-1. Pick a theme for the trivia game
-2. Create questions with 1 correct and 3 incorrect answers.
-3. Create score counters of correct and incorrect picks for each answer selected
-4. Create game states that determine which portion of the game is displayed and when.
-5. Create on-click events for buttons that correspond with the question that is displayed to determine if the user picked correctly of not.
-6. Create timers for each question that gives the user 30 seconds to pick an answer.
-7. Make timer move game to next stage whether the user picks correctly, incorrectly, or not at all.
-8. Create a win page for each correct guess.
-9. Create a loss page for each incorrect guess that displays the correct answer.
-10. Display users' correct, incorrect, and unanswered questions at the end of the game.
-Bonus. Play audio files for each correct or incorrect answer, as well as at the beginning and end of the game.
-*/
-/* Conditions of the game:
-1. User is given a question with 4 possible answers.
-2. User must select and answer by clicking a corresponding button.
-3. If answer is correct, a win screen is briefly displayed (an audio file is played) and the game moves to the next question
-4. If the answer is incorrect, a loss screen is displayed with the correct answer(an audio file is played) and the game moves to the next question.
-5. Each question the user is given 30 seconds to answer.
-6. At the end of the game, (another audio file is played), correct, incorrect, and unanswered questsions are displayed, and a reset button is displayed.
+//Function the ensures HTML loads first
+$(document).ready(function () {
+	//Object
+	const triviaGame = [{
+		question: "Who was Michael Scott's first on-screen kiss?",
+		options: ["Jan", "Carol", "Donna", "Holly"],
+		answer: 0,
+		photo1: "assets/images/jan.webp",
+		photo2: "assets/images/jan2.gif",
+	}, {
+		question: "What product does Dunder Mifflin primarily sell?",
+		options: ["Pencils", "Post-it Notes", "Paper", "Printers"],
+		answer: 2,
+		photo1: "assets/images/celebration.gif",
+		photo2: "assets/images/turntables.gif",
+	}, {
+		question: "What is the name of Jim and Pam's first child?",
+		options: ["Peepee", "Peepa", "Mimi", "Cece"],
+		answer: 3,
+		photo1: "assets/images/highfive.gif",
+		photo2: "assets/images/wrong.gif",
+	}, {
+		question: "Abraham Lincoln once said that 'If you're a racist...",
+		options: ["I will attack you with the north", "I hate you", "you'll go to prison", "don't call me Surly"],
+		answer: 0,
+		photo1: "assets/images/thankyou.gif",
+		photo2: "assets/images/ignorant.gif",
+	}, {
+		question: "In an episode titled 'The Injury', what injury does Michael endure?",
+		options: ["Head stuck in stair railing", "Burns foot on foreman grill", "Hit by a car", "Stomach Cramps"],
+		answer: 1,
+		photo1: "assets/images/correct.gif",
+		photo2: "assets/images/grill.gif",
+	}, {
+		question: "Who was Pam's first romance on the show?",
+		options: ["Dwight", "Oscar", "Roy", "Michael"],
+		answer: 2,
+		photo1: "assets/images/nice.gif",
+		photo2: "assets/images/work.gif",
+	}];
+	//Global Variables
+	let correctGuess = 0;
+	let incorrectGuess = 0;
+	let unanswered = 0;
+	let isRunning = false;
+	let timer = 30;
+	let intervalId;
+	let userGuess = "";
+	let qCount = triviaGame.length;
+	let pick;
+	let index;
+	let emptyArray = [];
+	let tempArray = [];
+	const themeSong = new Audio("assets/audio/officetheme.mp3");
 
-/*
-1. Make start screen appear that includes begin button
-2. Make question screen appear with 1 question, and 4 answers
-3. Determine if user clicks correct or incorrect answer.
-4. Display Correct screen, or incorrect screen that displays correct answer.
-5. Display end screen with correct, incorrect, and unanswered stats.
-6. Make play again button reset game without refreshing browser.
-*/
-//Creating an object with the question and answers
-const triviaGame = {
-	q1 : "Who was Michael Scott's first on-screen kiss?",
-	q1Answers : {
-		a1 : "Jan",
-		a2 : "Carol",
-		a3 : "Donna",
-		a4 : "Holly",
-	},
-	q2 : "What product does Dunder Mifflin primarily sell?",
-	q2Answers : {
-		a1 : "Pencils",
-		a2 : "Post-it Notes",
-		a3 : "Paper",
-		a4 : "Printers",
-	}
-}
-//Global variables I think I need
-let correctGuess = 0;
-let incorrectGuess = 0;
-let unanswered = 0;
-let isRunning = false;
-let themeSong = new Audio("assets/audio/officetheme.mp3");
-let questionCount = 0;
-//Function to ensure HTML loads first
-$(document).ready(function() {
-//Adding a button to make the game start and class for styling later
-$("#answers-stats").append("<h2> Click here to begin </h2>");
-$("h2").attr("id", "goBtn");
-//Variables for the countDown function
-let timer = 30;
-let timerId = setInterval(countDown, 1000);
-//Countdown function that gives player 30 seconds to answer question
-function countDown() {
-	if (!isRunning) {
-		return false;
-	} else if (timer === 0) {
-		clearTimeout(timerId);
-		timesUp();
-	} else {
-		$("#time-remaining").text(timer + " seconds remaining ");
-		timer--;
-	}
-}
-//Adding on click event to goBtn to start gaming
-$("#goBtn").on("click", function() {
-	isRunning = true;
-	quest1();
-});
-/*Creating a function for the question section,
-	that displays the question and the four answers,
-	by pulling values from object*/
-function quest1() {
-	let quest1 = $("#question-text").text(triviaGame.q1);
-		let ans1 = $("<h2>").text(triviaGame.q1Answers.a1);
-			ans1.addClass("right");
-		let ans2 = $("<h2>").text(triviaGame.q1Answers.a2);
-			ans2.addClass("wrong");
-		let ans3 = $("<h2>").text(triviaGame.q1Answers.a3);
-			ans3.addClass("wrong");
-		let ans4 = $("<h2>").text(triviaGame.q1Answers.a4);
-			ans4.addClass("wrong");	
-		if (isRunning = true) {
-			countDown();
-			$("#goBtn").text("");
-			$("#question-text").append(quest1);
-			$("#answers-stats").append(ans1, ans2, ans3, ans4);
-	}
-	//On-click function that determines if answers is correct or incorrect.
-	$(".right").on("click", function() {
-		corGuess();
-	});
-	//2nd on-click to determine if incorrect or correct
-	$(".wrong").on("click", function() {
-		incorGuess();
+	let goBtn = $("<h2>").text("Click here to begin");
+	$("#answers-stats").append(goBtn);
+	themeSong.play();
+
+	//Function that begins the game
+	$(goBtn).on("click", function () {
+		goBtn.hide();
+		countDown();
+		displayQuestion();
+		for (let i = 0; i < triviaGame.length; i++) {
+			tempArray.push(triviaGame[i]);
+		}
 	})
-}
-function quest2() {
-	let quest2 = $("#question-text").text(triviaGame.q2);
-		let ans1 = $("<h2>").text(triviaGame.q2Answers.a1);
-			ans1.addClass("wrong");
-		let ans2 = $("<h2>").text(triviaGame.q2Answers.a2);
-}
-//Function that displays when the 30 seconds are up.
-function timesUp() {
-	unanswered++;
-	$("#time-remaining").text("");
-	$("#question-text").text("You didn't answer the question!");
-	let idiotGif = $("<img>").attr("src", "assets/images/dwight.gif");
-	$("#answers-stats").text("");
-	$("#answers-stats").append(idiotGif);
-}
-//Function that displays when the user guesses correctly
-function corGuess() {
-	correctGuess++;
-	$("#question-text").text("Congratulations! You want a medal, or should we keep going?");
-	let correctGif = $("<img>").attr("src", "assets/images/jan.webp");
-	clearTimeout(timerId);
-	$("#time-remaining").text("");
-	$("h2").text("");
-	$("#answers-stats").append(correctGif);
-	if (questionCount = 1) {
-		setTimeout(quest2, 1000 * 3);
+	//Function that starts the timer 
+	function countDown() {
+		if (!isRunning) {
+			intervalId = setInterval(decrement, 1000);
+			isRunning = true;
+		}
 	}
-}
-//Function that displays when the user guesses incorrectly
-function incorGuess() {
-	incorrectGuess++;
-	$("#question-text").text("No, idiot. The correct answer was: " + triviaGame.q1Answers.a1);
-	let incorrectGif = $("<img>").attr("src", "assets/images/jan2.gif");
-	clearTimeout(timerId);
-	$("#time-remaining").text("");
-	$("h2").text("");
-	$("#answers-stats").append(incorrectGif);
-}
-})
+	//Function that begins the countdown
+	function decrement() {
+		$("#time-remaining").html("<h3>Time Remaining: " + timer + "</h3>");
+		timer--;
+		//Stops the timer if it reaches 0
+		if (timer === 0) {
+			unanswered++;
+			halt();
+			$("#question-text").html("<h3> You didn't answer the question...</h3>");
+			let idiotGif = $("<img>").attr("src", "assets/images/dwight.gif");
+			$("#answers-stats").text("");
+			$("#answers-stats").append(idiotGif);
+			unansweredPic();
+		}
+	}
+	//Function that stops the countDown, will be called multiple times.
+	function halt() {
+		isRunning = false;
+		clearInterval(intervalId);
+	}
+	//Function that displays the question randomly based on whether it has been displayed or not
+	function displayQuestion() {
+		//Randomizes a position in the object's array
+		index = Math.floor(Math.random() * triviaGame.length);
+		//Associates the choice with the random position chosen above
+		pick = triviaGame[index];
+		//Appends the chosen question to the question-text div
+		$("#question-text").html("<h3>" + pick.question + "</h3>");
+		//Loops through the keys in the chosen position
+		for (let i = 0; i < pick.options.length; i++) {
+			//Assigns the user's guess to a dynamically created div
+			let userGuess = $("<h2>").attr("class", "userguess");
+			//Appends each value as an h2 inside a newly created div
+			userGuess.html(pick.options[i]);
+			//Assigns a data-value to each key: value
+			userGuess.attr("data-guessvalue", i);
+			//Appends the newly created divs with each answer option into the answers-stats div
+			$("#answers-stats").append(userGuess);
+		}
+
+		//Function that allows user to choose an answer and determines the outcome
+		$(".userguess").on("click", function () {
+			//Determines the array position of each userGuess option by converting it back to an integer temporarily
+			userGuess = parseInt($(this).attr("data-guessvalue"));
+			//If user guesses correctly
+			if (userGuess === pick.answer) {
+				//Stop the countDown
+				halt();
+				//Add to the correctGuess counter
+				correctGuess++;
+				//Temporarily empty the userGuess text
+				userGuess = "";
+				//Add congratulatory text to question-text div
+				$("#question-text").html("<h3> Congrats! You want a medal, or should we continue? </h3>");
+				//Call correct guess picture function
+				correctPic();
+			} else {
+				halt();
+				incorrectGuess++;
+				userGuess = "";
+				$("#question-text").html("<h3> Nope, the correct answer is: " + pick.options[pick.answer] + "</h3>");
+				incorrectPic();
+			}
+		})
+	}
+	//Function that hides photo and then displays if anwer is correct
+	function correctPic() {
+		$("#answers-stats").html("<img src=" + pick.photo1 + ">");
+		emptyArray.push(pick);
+		//.splice researched here: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice"
+		triviaGame.splice(index, 1);
+		//Assigns variable to set timeout
+		let hiddenPic = setTimeout(function () {
+			$("#answers-stats").empty();
+			timer = 30;
+			//Check if game is over, and then displays the score screen
+			if ((incorrectGuess + correctGuess + unanswered) === qCount) {
+				//Empty's the question-text div
+				$("#question-text").empty();
+				//Overrides question-text with:
+				$("#question-text").html("<h3> Well, that's every of the time we have; here's how ya did: </h3>");
+				//Appends correct, incorrect, and unanswered scores, appends reset button, and resets scores to zero, or continues the game
+				$("#answers-stats").append("<h2> Correct: " + correctGuess + "</h2>");
+				$("#answers-stats").append("<h2> Incorrect: " + incorrectGuess + "</h2>");
+				$("#answers-stats").append("<h2> Unanswered: " + unanswered + "</h2>");
+				let resetBtn = $("<h4>").text("Play Again?");
+				resetBtn.attr("id", "reset");
+				$("#answers-stats").append(resetBtn);
+				correctGuess = 0;
+				incorrectGuess = 0;
+				unanswered = 0;
+			} else {
+				countDown();
+				displayQuestion();
+			}
+		}, 1000 * 3);
+	}
+	//Function that hides photo and then displays if answer is incorrect
+	function incorrectPic() {
+		$("#answers-stats").html("<img src=" + pick.photo2 + ">");
+		emptyArray.push(pick);
+		//.splice researched here: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice"
+		triviaGame.splice(index, 1);
+		//Assigns variable to set timeout
+		let hiddenPic = setTimeout(function () {
+			$("#answers-stats").empty();
+			timer = 30;
+			//Check if game is over, and then displays the score screen
+			if ((incorrectGuess + correctGuess + unanswered) === qCount) {
+				//Empty's the question-text div
+				$("#question-text").empty();
+				//Overrides question-text with:
+				$("#question-text").html("<h3> Well, that's every of the time we have; here's how ya did: </h3>");
+				//Appends correct, incorrect, and unanswered scores, appends reset button, and resets scores to zero, or continues the game
+				$("#answers-stats").append("<h2> Correct: " + correctGuess + "</h2>");
+				$("#answers-stats").append("<h2> Incorrect: " + incorrectGuess + "</h2>");
+				$("#answers-stats").append("<h2> Unanswered... " + unanswered + "</h2>");
+				let resetBtn = $("<h4>").text("Play Again?");
+				resetBtn.attr("id", "reset");
+				$("#answers-stats").append(resetBtn);
+				correctGuess = 0;
+				incorrectGuess = 0;
+				unanswered = 0;
+			} else {
+				countDown();
+				displayQuestion();
+			}
+		}, 1000 * 3);
+	}
+	//Function that hides photo then displays if question isn't answered
+	function unansweredPic() {
+		const fail = $("<img>").attr("src", "assets/images/dwight.gif");
+		$("#answers-stats").html(fail);
+		emptyArray.push(pick);
+		//.splice researched here: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice"
+		triviaGame.splice(index, 1);
+		//Assigns variable to set timeout
+		let hiddenPic = setTimeout(function () {
+			$("#answers-stats").empty();
+			timer = 30;
+			//Check if game is over, and then displays the score screen
+			if ((incorrectGuess + correctGuess + unanswered) === qCount) {
+				//Empty's the question-text div
+				$("#question-text").empty();
+				//Overrides question-text with:
+				$("#question-text").html("<h3> Well, that's every of the time we have; here's how ya did: </h3>");
+				//Appends correct, incorrect, and unanswered scores, appends reset button, and resets scores to zero, or continues the game
+				$("#answers-stats").append("<h2> Correct: " + correctGuess + "</h2>");
+				$("#answers-stats").append("<h2> Incorrect: " + incorrectGuess + "</h2>");
+				$("#answers-stats").append("<h2> Unanswered... " + unanswered + "</h2>");
+				let resetBtn = $("<h4>").text("Play Again?");
+				resetBtn.attr("id", "reset");
+				$("#answers-stats").append(resetBtn);
+				correctGuess = 0;
+				incorrectGuess = 0;
+				unanswered = 0;
+			} else {
+				countDown();
+				displayQuestion();
+			}
+		}, 1000 * 3);
+	}
+	//Function that restarts the game
+	$("#reset").on("click", function () {
+		$("#answers-stats").empty();
+		$("#question-text").empty();
+		for (let i = 0; i < tempArray.length; i++) {
+			triviaGame.push(tempArray[i]);
+		}
+		countDown();
+		displayQuestion();
+	})
+
+});
